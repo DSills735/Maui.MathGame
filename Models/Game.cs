2f6878668938b70f1e32eb71fe18bb42d64de4f1
David Sills <david.sills100@gmail.com>
2026-06-14 16:28:07 -0500

    added game model

---
+++ Models/Game.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Maui.MathGame.Models;
+
+public class Game
+{
+    public int Id { get; set; }
+    public GameOperation Type { get; set; }
+    public int Score { get; set; }
+    public DateTime Dateplayed { get; set; }
+}
+
+public enum GameOperation
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+}
