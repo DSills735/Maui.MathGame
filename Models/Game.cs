683cec40aa5e378d1ee9bf5ade9c206d80aedb6a
David Sills <david.sills100@gmail.com>
2026-06-14 16:30:14 -0500

    updated model

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,8 +1,11 @@
-﻿
+﻿using SQLite;
+
 namespace Maui.MathGame.Models;
 
+[Table("game")]
 public class Game
 {
+    [PrimaryKey, AutoIncrement, Column("Id")]
     public int Id { get; set; }
     public GameOperation Type { get; set; }
     public int Score { get; set; }
